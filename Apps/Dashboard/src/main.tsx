0b2277c9d050148803b2d2d4826045c5738b6cce
Dev Rafael <devrafaael@gmail.com>
2025-12-06 13:22:49 -0300

    Transformar Front End em pasta normal do monorepo

--- Apps/Dashboard/src/main.tsx
+++ Apps/Dashboard/src/main.tsx
@@ -2,45 +2,52 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+
 import Login from './app/pages/Login'
-import Metricas from './app/pages/Metricas/metricas'
 import Produtos from './app/pages/admin/pizzasManage'
-import PizzasManage from './app/pages/admin/pizzasManage'
+import Metricas from './app/pages/Metricas/metricas'
 import UserManage from './app/pages/admin/userManage'
+import PizzasManage from './app/pages/admin/pizzasManage'
 import AddressManage from './app/pages/admin/AddressManage'
 import AdicionaisManage from './app/pages/admin/AdicionaisManage'
 import OrderItemManage from './app/pages/admin/OrderItemManage'
 import PerfilManage from './app/pages/admin/PerfilManage'
-import PedidosManage from './app/pages/admin/orderManage'
 import PrivateRoute from './middlewares/PrivateRoute'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import PedidosManage from './app/pages/admin/orderManage'
+
+
 
 const router = createBrowserRouter([
-  { path: "/login", element: <Login /> },
+ { path: "/login", element: <Login /> },
   {
     path: "/",
     element: <App />,
     children: [
       {
-        element: <PrivateRoute />,
+         element: <PrivateRoute />,
         children: [
-          { path: "dashboard", element: <Metricas /> },
-          { path: "pedidos", element: <PedidosManage /> },
-          { path: "produtos", element: <Produtos /> },
-          { path: "pizzas", element: <PizzasManage /> },
-          { path: "usuarios", element: <UserManage /> },
-          { path: "enderecos", element: <AddressManage /> },
-          { path: "adicionais", element: <AdicionaisManage /> },
-          { path: "ordersitems", element: <OrderItemManage /> },
-          { path: "perfil", element: <PerfilManage /> },
+          { path: "/dashboard", element: <Metricas /> },
+          { path: "/Pedidos", element: <PedidosManage /> },
+          { path: "/produtos", element: <Produtos /> },
+          { path: "/Pizzas", element: <PizzasManage /> },
+          { path: "/Usuarios", element: <UserManage /> },
+          { path: "/Endereços", element: <AddressManage /> },
+          { path: "/Adicionais", element: <AdicionaisManage /> },
+          { path: "/OrdersItems", element: <OrderItemManage /> },
+          { path: "/perfil", element: <PerfilManage /> },
+
         ],
       },
     ],
   },
-])
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   </StrictMode>
-)
+);
