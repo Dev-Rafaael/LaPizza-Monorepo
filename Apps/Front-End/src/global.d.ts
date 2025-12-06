c3bc9f57eb3a61c532c703c64c3d9e56ce157993
Dev Rafael <devrafaael@gmail.com>
2025-12-06 11:42:57 -0300

    Adicionar declarações globais para CSS e imagens

--- Apps/Front-End/src/global.d.ts
+++ Apps/Front-End/src/global.d.ts
@@ -1,4 +1,23 @@
+// Para CSS Modules
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
+}
+
+// Para imagens
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+declare module '*.svg' {
+  const value: string;
+  export default value;
 }
